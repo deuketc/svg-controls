1d20176874a122134b86c719836081f28f014204
deuketc <charles.deukett@hotmail.com>
2018-10-28 19:55:40 +1300

    more svg animation

--- src/Components/Controls/UI/TextInputIcon/TextInputIcon.js
+++ src/Components/Controls/UI/TextInputIcon/TextInputIcon.js
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, {Component} from 'react'
 import styles from './TextInputIcon.module.css'
+import {TweenMax} from "gsap/TweenMax";
 
-function TextInputIcon(props) {
-  return (
-    <div className={styles.IconContainer}>
-        <span className={styles.IconNumber}>{props.IconIndex}</span>
-    </div>
-  )
+class TextInputIcon extends Component {
+
+  constructor(props){
+    super(props);
+
+    //this.myElement = null;
+    this.myCircle = null;
+
+    //this.myTween = null;
+    this.animation = null
+ 
+  }
+
+  componentDidMount(){
+    // use the node ref to create the animation
+    //this.myTween = TweenMax.to(this.myElement, 1, {x: 100, y: 100});
+    
+    /*
+    this.animation = new TimelineMax({paused: true });
+
+    this.animation.to(this.myCircle, 1, {ease: Power2.easeOut, strokeDashoffset:0});
+
+    this.animation.play(); 
+    */
+   this.myTween = TweenMax.set(this.myElement, {strokeDashoffset:"176"});
+   this.myTween = TweenMax.to(this.myElement, .85, {rotationZ:"360deg", repeat:2, delay:.4, ease:'Linear.easeIn'});
+
+   this.myTween = TweenMax.to(this.myCircle, .5, {strokeDashoffset:"90", delay:.2, ease:'Linear.easeIn'});
+   this.myTween = TweenMax.to(this.myCircle, .41, {strokeDashoffset:"176", delay:1.6, ease:'Linear.easeIn'});
+
+   this.myTween = TweenMax.to(this.myElement2, .8, {strokeDashoffset:"-12", delay:1.6, ease:'Linear.easeIn'});
+
+  }
+
+  render(){
+    return (
+      <div className={styles.IconContainer}>
+          
+          <svg ref={svg => this.myElement2 = svg} className={styles.Tick} height="60" width="60">
+            <polyline fill="none" stroke="#2195F3" strokeWidth="4" strokeLinecap="round" strokeMiterlimit="10" points="52.447,14.359 
+            25.614,41.192 17.389,32.968 "/>
+            </svg>
+
+          <svg className={styles.Loader} ref={svg => this.myElement = svg} height="60" width="60">
+            <circle  ref={circle => this.myCircle = circle}  cx="30" cy="30" r="28" stroke="#2195F3" strokeLinecap="round" strokeWidth="4" fill="none" />
+          </svg>
+
+          <span className={styles.IconNumber}>{this.props.IconIndex}</span>
+      </div>
+    )
+  }
 }
 
 
---
+++ src/Components/Controls/UI/TextInputIcon/svg/loading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ function loading() {
+  return (
+	<svg version="1.1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
+		width="60px" height="60px" viewBox="0 0 60 60" enable-background="new 0 0 60 60">
+			<path fill="#2195F3" d="M30,4c14.337,0,26,11.664,26,26c0,14.337-11.663,26-26,26C15.664,56,4,44.337,4,30C4,15.664,15.664,4,30,4
+				M30,0C13.432,0,0,13.431,0,30c0,16.568,13.432,30,30,30c16.569,0,30-13.432,30-30C60,13.431,46.569,0,30,0L30,0z"/>
+	</svg>
+
+  )
+}
+
+export default loading;
